9d4c5cf489ea38a13cbca2500f518d1983a086b5
wwhgtt <1725293885@qq.com>
2015-05-04 15:33:29 +0800

    Revert "yj-web-platform"
    This reverts commit 1bd5db82da3bed568ea0cf50b57ec26fb0650079.

--- yj-web-platform/js/controllers/login.js
+++
@@ -1,4 +0,0 @@
-angular.module("controllers.login",[])
-.controller("Login",function($scope,$init){
-	console.log("$scope %o",$scope)
-})
--- yj-web-platform/js/controllers/systemInit.js
+++
@@ -1,4 +0,0 @@
-angular.module("controllers.systemInit",[])
-.controller("SystemInit",function($scope,$init){
-	console.log("$scope %o",$scope)
-})
