945e2a2e541135f0234bc77b72ee42924bb0d628
wwhgtt <1725293885@qq.com>
2015-05-25 17:48:23 +0800

    分批次

--- yi-web-driveschool/js/controllers/addMuch.js
+++ yi-web-driveschool/js/controllers/addMuch.js
@@ -1,8 +1,13 @@
 angular.module("controllers.addMuch",[])
 .controller("addMuch",function(
 	$scope,
-	$addAll
+	$addAll,
+	$addCar,
+	$bindcar
 ){
+	var idNumber=[];
+	var _id=[];
+	var resultCoachList = [];
 	$scope.coach = {
 		useworker:{disabled:false,checked:true},
 		xferable:{disabled:false,checked:true},
@@ -12,7 +17,7 @@ angular.module("controllers.addMuch",[])
 	$scope.addAll=function(){
 		var coachList=$scope.coachList;
 		var dataList=[];
-		for(index in coachList){
+		for(var index in coachList){
 			var name=coachList[index].name,
 			    phone=coachList[index].phone,
 			    idNumber=coachList[index].idNumber,
@@ -20,16 +25,92 @@ angular.module("controllers.addMuch",[])
 			var obj={"name":name,"phone":phone,"idNumber":idNumber,"type":type};
 			dataList[index]=obj;
 		}
-		// console.log(dataList);	
-		$addAll.addAll(dataList,function(err,result){
+		var perNum = 100;
+		var totalUploadTime = Math.ceil(dataList.length / perNum);
+		var currentUpload = 0;//当前传到第几组
+		var uploadCoach = function(i){
+			var dataList = dataList.slice(preNum*i,preNum*(i+1));
+			$addAll.addAll(dataList,function(err,result){
+				if(err){
+					//进行错误处理,
+					alert("sorry,系统出错");
+				}else{
+					if(result && result.coachList){
+						var infoList = result.coachList;
+						for(var index in infoList){
+							var temp = {idNumber:infoList[index].idNumber,coachId:infoList[index]._id};
+							resultCoachList.push(temp);
+						}
+						if(++currentUpload < totalUploadTime){
+							uploadCoach(currentUpload);
+						}else{
+							//说明几组数据都全部上传完成
+							alert("提交成功");
+						}
+					}
+				}
+			})
+		}
+		if(totalUploadTime > 0){
+			uploadCoach(currentUpload);
+		}else{
+			//空数据提示
+			alert("您传入的数据为空")
+		}
+	}
+	$scope.cars = {
+		useworker:{disabled:false,checked:true},
+		xferable:{disabled:false,checked:true},
+		userabs:{disabled:false,checked:true}
+	};
+	$scope.carList = [];
+	$scope.addCar=function(){
+		var coachList=$scope.coachList;
+		var carList=$scope.carList;
+		for(index in coachList){
+			var car=coachList[index].car
+			var obj={"name":car};
+			carList[index]=obj;
+		}
+		$addCar.addCar(carList,function(err,result){
 			if(err){
 				alert("sorry,提交失败");
 			}else{
-				if(result && result.errorList){ 
-					//$location.path("/split"); 
+				if(result && result.errorList == null){ 
 					alert("提交成功");  
 				}else{
-					console.log("请确认格式正确"); 
+					 $scope.errorList=result.errorList;
+				}
+			}
+		})
+	}
+	var bindList=[];
+    $scope.bindCar=function(){
+		var bindCarList=$scope.bindCarList;
+		for(index in resultCoachList){
+			var coachIdNum=resultCoachList[index].idNumber;
+            var coachid=resultCoachList[index].coachId;
+            var idNumber=coachList[index].idNumber;
+            var car=coachList[index].car;
+            if (idNumber == coachIdNum) {
+            	$scope.bindCar._id=coachid;
+            	$scope.bindCar.car=car;
+            	var _idFinaly=$scope.bindCar._id;
+            	var _car=$scope.bindCar.car;
+            	var carList={name:_car}
+            	var temp={coachId:_idFinaly,car:carList}
+            	bindList.push(temp);
+            };
+		}
+		$bindcar.bindcar(bindList,function(err,result){
+			if(err){
+				alert("sorry,提交失败");
+			}else{
+				if(result && result.errorList == null){ 
+					//$location.path("/split"); 
+					alert("绑定成功");  
+				}else{
+					 // $scope.errorList=result.errorList;
 				}
 			}
 		})
