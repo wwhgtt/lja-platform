2a6a79ce4ef3515c6e5f82a443be9bc14541d3c2
wwhgtt <1725293885@qq.com>
2015-06-06 18:14:36 +0800

    lastPage

--- yj-web-platform/js/controllers/userItem.js
+++ yj-web-platform/js/controllers/userItem.js
@@ -1,16 +1,18 @@
 angular.module("controllers.userItem",[]) 
 .controller("userItem",function(
 	$scope,
-	$getUserItem
+	$getUserItem,
+	$getLastUser
 ){
 	var top=1;
+	$scope.user={_id:"",phone:"",createDate:""};
 	$getUserItem.getUserItem(top,function(err,result){ 
 		if(err){
 			alert("sorry,访问出错");
 		}else{
 			if(result && result.userList !== null){
-				$scope.userItem=result.userItem;
-				alert("获取成功") 
+				$scope.userItem=result.userList;
+				alert("获取成功");
 			}else{
 				if(result && result.userList == null){
 					alert("用户为空");
@@ -18,5 +20,37 @@ angular.module("controllers.userItem",[])
 			}
 		}
 	})
-	
+	$scope.getFirstPage=function(){
+		$getUserItem.getUserItem(top,function(err,result){ 
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.userList !== null){
+					$scope.userItem=result.userList;
+					alert("获取成功");
+				}else{
+					if(result && result.userList == null){
+						alert("用户为空");
+					}
+				}
+			}
+		})
+	}
+	$scope.getLastPage=function(){
+		var top=200;
+		$getLastUser.getLastUser(top,function(err,result){
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.userList !== null){
+					$scope.userItem=result.userList;
+					alert("获取成功");
+				}else{
+					if(result && result.userList == null){
+						alert("用户为空");
+					}
+				}
+			}
+		})
+	}
 })
