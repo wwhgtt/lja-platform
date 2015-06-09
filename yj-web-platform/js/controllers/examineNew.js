f233f4e8a86c5439f1d7fb46b108e246b99beb34
wwhgtt <1725293885@qq.com>
2015-06-09 16:59:00 +0800

    审核改进

--- yj-web-platform/js/controllers/examineNew.js
+++ yj-web-platform/js/controllers/examineNew.js
@@ -3,12 +3,53 @@ angular.module("controllers.examineNews",[])
 	$scope,
 	$examine,
 	$window,
-	$resaon
+	$resaon,
+	$getDown
 ){
-    $scope.examine={id:"",teachType:"",name:"",resaon:""};
+    $scope.examine={id:"",teachType:"",name:"",resaon:"",urlCoach:"",userId:"",urlID:"",urlOther:"",urlCoach:"",urlIDa:"",urlOthera:""};
 	$scope.examines=function(imuser){
 	    $scope.showExamine = true;
 	    $scope.showReason = false;
+	    var lisenceType="coach";
+		var userId=imuser.userId;
+		$getDown.getDown(lisenceType,userId,function(err,result){
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.success == true){
+					console.log("成功");
+					$scope.examine.urlCoacha=result.downloadUrl;
+				}else{
+					alert("出错了");
+				}
+			}
+		})
+		var lisenceType="identity";
+		$getDown.getDown(lisenceType,userId,function(err,result){
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.success == true){
+					console.log("成功");
+					$scope.examine.urlIDa=result.downloadUrl;
+				}else{
+					alert("出错了");
+				}
+			}
+		})
+		var lisenceType="drive";
+		$getDown.getDown(lisenceType,userId,function(err,result){
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.success == true){
+					console.log("成功");
+					$scope.examine.urlOthera=result.downloadUrl;
+				}else{
+					alert("出错了");
+				}
+			}
+		})
 		$scope.examineCoach=function(){
 			var idNumber=$scope.examine.id,
 			teachType=$scope.examine.teachType,
@@ -32,6 +73,46 @@ angular.module("controllers.examineNews",[])
 	$scope.examineRefuseCoach=function(imuser){
 		$scope.showExamine = false;
 		$scope.showReason = true;
+		var lisenceType="coach";
+		var userId=imuser.userId;
+		$getDown.getDown(lisenceType,userId,function(err,result){
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.success == true){
+					console.log("成功");
+					$scope.examine.urlCoach=result.downloadUrl;
+				}else{
+					alert("出错了");
+				}
+			}
+		})
+		var lisenceType="identity";
+		$getDown.getDown(lisenceType,userId,function(err,result){
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.success == true){
+					console.log("成功");
+					$scope.examine.urlID=result.downloadUrl;
+				}else{
+					alert("出错了");
+				}
+			}
+		})
+		var lisenceType="drive";
+		$getDown.getDown(lisenceType,userId,function(err,result){
+			if(err){
+				alert("sorry,访问出错");
+			}else{
+				if(result && result.success == true){
+					console.log("成功");
+					$scope.examine.urlOther=result.downloadUrl;
+				}else{
+					alert("出错了");
+				}
+			}
+		})
 		$scope.examineResaon=function(){
 			var reason=$scope.examine.resaon;
 			var coachId=imuser.userId;
